af62b0c79fd2a983b85742f31f3867853e83a65c
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2014-09-06 14:05:31 -0400

    Initial commit

---
+++ mrbavii/__init__.py
@@ -0,0 +1,4 @@
+# For the time being, this file is here.  As per PEP 420, it will eventually be moved.
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    version = '0.0',
+    namespace_packages = ["mrbavii"],
+    name = 'mrbavii.pysite',
+    description = 'A set of Python help classes for WSGI applications',
+    install_requires = ['setuptools']
+)
+
