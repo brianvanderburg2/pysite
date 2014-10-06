91fd55af5f9beee1126efe40787c8bd9816a21f7
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2014-10-05 22:15:07 -0400

    Added u() and b() compatibility functions.
    In order to be compatible with Python 2 and 3, two functions have been
    added.  A unicode string can be constructed as:
    
        from .compat import u
        u('string here just as with u"..."')
    
    Also, a bytes string can be constructed as:
    
        from .compat import b
        b('string here just as with b"..."')
    
    While the bytes string isn't needed (b"..." works in Python 2 and 3), it
    is provded for consistency.

--- mrbavii/pysite/compat.py
+++ mrbavii/pysite/compat.py
@@ -7,9 +7,26 @@ import sys
 PY2 = sys.version[0] == '2'
 PY3 = sys.version[0] == '3'
 
+
 if PY2:
-    pass
+    import codecs
+
+    def u(s):
+        """ Return the unicode version of teh string. """
+        return codecs.unicode_escape_decode(s)[0]
+
+    def b(s):
+        """ Return the string as bytes. """
+        return s
 else:
-    pass
+    import codecs
+
+    def u(s):
+        """ Return the unicode version of the string. """
+        return s
+
+    def b(s)
+        """ Return the string as bytes. """
+        return codecs.unicode_escape_encode(s)[0]
 
 
